bb00368a4601847db6998b0a6f6ddea41adf638d
Valentin Churavy <v.churavy@gmail.com>
2013-10-11 14:08:00 +0900

    maven initialization

---
+++ src/main/scala/org/vastness/evo2dsim/App.scala
@@ -0,0 +1,13 @@
+package org.vastness.evo2dsim
+
+/**
+ * @author Valentin Churavy 
+ */
+object App {
+  
+  
+  def main(args : Array[String]) {
+    println( "Evo2DSim is a simple simulator for evolutionary experiments." )
+  }
+
+}
