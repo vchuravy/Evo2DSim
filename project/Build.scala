2778be1e902a58255b2ffa77322d427fd270ee59
Valentin Churavy <v.churavy@gmail.com>
2013-11-26 12:42:05 +0900

    adds sbt files

---
+++ project/Build.scala
@@ -0,0 +1,8 @@
+
+object MyBuild extends Build {
+
+  lazy val root = Project("root", file("."))
+    .dependsOn(scala_utils)
+
+  lazy val scala_utils = RootProject(uri("git://github.com/wallnuss/scala-utils.git"))
+}
