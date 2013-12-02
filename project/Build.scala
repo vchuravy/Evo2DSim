8387b219c779dd68334f7e4b77c2acbfcc5422a3
Valentin Churavy <v.churavy@gmail.com>
2013-12-02 12:27:33 +0900

    adds org.vastness.utils as dependency

--- project/Build.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-
-object MyBuild extends Build {
-
-  lazy val root = Project("root", file("."))
-    .dependsOn(scala_utils)
-
-  lazy val scala_utils = RootProject(uri("git://github.com/wallnuss/scala-utils.git"))
-}
