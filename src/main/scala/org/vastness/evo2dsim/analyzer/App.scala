4d4e7c2e6b0066981ad00c7e2375453b70a637a3
Valentin Churavy <v.churavy@gmail.com>
2013-12-02 21:36:06 +0900

    add basic gui

--- src/main/scala/org/vastness/evo2dsim/analyzer/App.scala
+++ src/main/scala/org/vastness/evo2dsim/analyzer/App.scala
@@ -4,20 +4,24 @@ import org.vastness.evo2dsim.gui._
 import java.util.Timer
 import spray.json._
 import org.vastness.evo2dsim.utils.MyJsonProtocol._
-import javax.swing.{SwingUtilities, JFrame}
+import javax.swing.SwingUtilities
+import scala.swing.Frame
 import scalax.file.Path
-import scalax.io.{Input, Resource}
-import org.vastness.evo2dsim.evolution
+import scalax.io.Input
+import java.io.File
 import org.vastness.evo2dsim.evolution.Genome
+import org.vastness.evo2dsim.environment.BasicEnvironment
+import scala.concurrent._
+import ExecutionContext.Implicits.global
 
-class App {
+object App {
   var timer = new Timer
   var running = true
   val HERTZ = 30
   val gui = new GUI
 
   private def render() {
-    gui.getWorldView.repaint()
+    gui.worldView.repaint()
   }
 
   def loop() {
@@ -43,35 +47,76 @@ class App {
       head("Evo2DSim is a simple simulator for evolutionary environment.")
       opt[Int]('t', "timeStep") action { (x, c) =>
         c.copy(timeStep = x) } text "Time step in ms"
-      opt[String]('g', "generations") action { (x, c) =>
+      opt[File]('p', "path") action { (x, c) =>
         c.copy(path = x) } text "Evaluation directory"
     }
 
     parser.parse(args, Config()) map { config =>
-    SwingUtilities.invokeLater(new Runnable() {
+
+     SwingUtilities.invokeLater(new Runnable() {
        override def run() {
-         val frame: JFrame = new JFrame("GUI")
-         frame.setContentPane(gui.getPanel)
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+         val frame: Frame = new Frame() {
+           override def contents = Seq(gui.panel)
+         }
          frame.pack()
-         frame.setVisible(true)
        }
      })
 
-
     loop() // starting render loop
+    tui(config.path, config.timeStep)
     } getOrElse {
       sys.exit(1)
       // arguments are bad, usage message will have been displayed
     }
   }
 
-  case class Config(timeStep: Int = 50, path: String = "")
+  case class Config(timeStep: Int = 50, path: File = new File("."))
 
   def loadGeneration(dir: Path, generation: Int): Map[Int, (Double, Genome)] = {
     val genFile = dir resolve "Gen_%04d.json".format(generation)
-    val input: Input = Resource.fromInputStream(genFile.inputStream())
+    val input: Input = genFile.inputStream()
     val gen = input.string.asJson.convertTo[Map[String, (Double, Genome)]]
-    gen.map(x => x._1.toInt -> x._2)
+    gen.map (x => x._1.toInt -> x._2)
+  }
+
+  def loadStats(dir: Path): List[(Int, (Double, Double, Double, Double))] = {
+    val statsFile = dir resolve "Stats.csv"
+    val in = statsFile.inputStream()
+    val csv: Traversable[Array[String]] = in.lines().map (_ split ',').tail
+    csv.map {
+      case Array(index, max, min, mean, variance) => index.toInt -> (max.toDouble, min.toDouble, mean.toDouble, variance.toDouble)
+    }.toList
+  }
+
+  def findBestGroupInGen(gen: Map[Int, (Double, Genome)], size: Int) = {
+    val groups = gen.toList.sortBy(_._1).grouped(size)
+    val groupPerformance = groups.map(e => e.foldLeft[Double](0.0){ case (acc, (_, (fitness, _))) => acc + fitness}).toIndexedSeq
+    ( for(i <- groupPerformance.indices) yield (i, groupPerformance(i) / size) ).toList.sortBy(_._2)
+  }
+
+  def tui(path: File, timeStep: Int) {
+    val dir = Path(path)
+    val stats = loadStats(dir)
+    for((index,(max, min, mean, v)) <- stats.sortBy(_._2._3)) {
+          printf("Gen %04d, max: %.2f, min: %.2f, mean: %.2f, var: %.2f \n", index, max, min,mean ,v)
+    }
+    print("Please select a generation: ")
+    val genIndex = readInt()
+    val gen = loadGeneration(dir, genIndex)
+
+    for((index, gP) <- findBestGroupInGen(gen, 10)) {
+      printf("GI: %4d, %.2f \n", index, gP)
+    }
+    print("Select a group to evaluate: ")
+    val group = readInt()
+
+    val e = new BasicEnvironment(timeStep, 0)
+    e.initializeStatic()
+    e.initializeAgents(gen.grouped(10).toIndexedSeq(group))
+    EnvironmentManager.addEnvironment(e)
+    future {
+      e.run()
+    }
+
   }
 }
