3eea320218b80372b316db68a8184e9158ec8359
Valentin Churavy <v.churavy@gmail.com>
2013-12-03 11:36:53 +0900

    removes gui from App

--- src/main/scala/org/vastness/evo2dsim/App.scala
+++ src/main/scala/org/vastness/evo2dsim/App.scala
@@ -1,41 +1,12 @@
 package org.vastness.evo2dsim
 
-import org.vastness.evo2dsim.gui._
-import java.util.Timer
+
 import org.vastness.evo2dsim.evolution.SUSEvolution
 
 /**
  * @author Valentin Churavy
  */
 object App {
-  var timer = new Timer
-  var running = true
-  val HERTZ = 30
-  val gui = new GUI
-
-  private def render() {
-    gui.worldView.repaint()
-  }
-
-  def loop() {
-    timer = new Timer()
-    timer.schedule(new RenderLoop, 0, 1000 / HERTZ)//new timer at 30 fps, the timing mechanism
-  }
-
-  private class RenderLoop extends java.util.TimerTask
-  {
-    override def run()
-    {
-      render()
-
-      if (!running)
-      {
-        timer.cancel()
-      }
-    }
-  }
-
-
   def main(args : Array[String]) {
     val parser = new scopt.OptionParser[Config]("scopt") {
       head("Evo2DSim is a simple simulator for evolutionary environment.")
@@ -54,18 +25,8 @@ object App {
     }
 
     parser.parse(args, Config()) map { config =>
-     /* SwingUtilities.invokeLater(new Runnable() {
-        override def run() {
-          val frame: JFrame = new JFrame("GUI")
-          frame.setContentPane(gui.getPanel)
-          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-          frame.pack()
-          frame.setVisible(true)
-        }
-      }) */
 
       val evo = new SUSEvolution(config.numberOfIndiviums, config.groupSize, config.stepsPerEvaluation, config.generation, config.evaluationPerGeneration, config.timeStep)
-      //loop() // starting render loop
       evo.start()
     } getOrElse {
       sys.exit(1)
@@ -74,5 +35,5 @@ object App {
   }
 
 
-  case class Config(timeStep: Int = 50, generation: Int = 500, stepsPerEvaluation: Int = 1, evaluationPerGeneration:Int = 1,  numberOfIndiviums:Int = 2000, groupSize: Int = 10)
+  case class Config(timeStep: Int = 50, generation: Int = 500, stepsPerEvaluation: Int = 5000, evaluationPerGeneration:Int = 5,  numberOfIndiviums:Int = 2000, groupSize: Int = 10)
 }
