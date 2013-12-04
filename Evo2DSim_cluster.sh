45cbebdd53fdd0823973fd11f719fb38ed941e97
Valentin Churavy <v.churavy@gmail.com>
2013-12-04 23:33:00 +0900

    use long and multicore

--- Evo2DSim_cluster.sh
+++ Evo2DSim_cluster.sh
@@ -1,14 +1,15 @@
 # This is a simple SGE batch script you can run at OIST
-# 
-# request Bash shell as shell for job 
-#$ -S /bin/bash 
+#
+# request Bash shell as shell for job
+#$ -S /bin/bash
 #$ -N Evo2DSimVCHURAVY
-# 
+#
 # Set all the variables SGE needs to run the job
-#$ -q short 
-#$ -M valentin-churavy@oist.jp 
-#$ -m abe 
-#$ -j yes 
+#$ -q longP
+#$ -pe openmp 8
+#$ -M valentin-churavy@oist.jp
+#$ -m abe
+#$ -j yes
 #$ -l h_vmem=10g
 #$ -l virtual_free=4g
 
