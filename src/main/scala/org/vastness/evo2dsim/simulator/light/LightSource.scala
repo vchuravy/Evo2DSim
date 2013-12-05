f4f3c4acc0ba4de22801fa85b947c22252902954
Valentin Churavy <v.churavy@gmail.com>
2013-12-05 11:55:15 +0900

    adds LGPL

--- src/main/scala/org/vastness/evo2dsim/simulator/light/LightSource.scala
+++ src/main/scala/org/vastness/evo2dsim/simulator/light/LightSource.scala
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Evo2DSim.
+ *
+ * Evo2DSim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Evo2DSim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Evo2DSim.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.vastness.evo2dsim.simulator.light
 
 import org.vastness.evo2dsim.simulator.Entity
@@ -16,8 +33,5 @@ class LightSource(c: Color, entity: Entity, val category: LightCategory) {
   var active_ = false
   def forced_disable = entity.sim.lightManager.disabledCategories.contains(category)
 
-
   def color = if(active) c else Color.BLACK
-
-
 }
