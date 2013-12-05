9cd48edd666875181c36bf1c6e305985ad0e4c01
Valentin Churavy <v.churavy@gmail.com>
2013-12-05 11:44:30 +0900

    adds License

--- src/main/scala/org/vastness/evo2dsim/analyzer/App.scala
+++ src/main/scala/org/vastness/evo2dsim/analyzer/App.scala
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Evo2DSimAnalyzer.
+ *
+ * Evo2DSimAnalyzer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Evo2DSimAnalyzer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Evo2DSimAnalyzer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.vastness.evo2dsim.analyzer
 
 import org.vastness.evo2dsim.gui._
