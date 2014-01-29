8901972e368716717f71cea99eab9e7cc6b7bdb0
Valentin Churavy <v.churavy@gmail.com>
2014-01-29 16:38:43 -0500

    update data scripts to run over selected few settings

--- scripts/DataRunner.sh
+++ scripts/DataRunner.sh
@@ -7,9 +7,15 @@ FILE="Data_cluster.sh"
 CMD="${BASECMD} ${SCRIPTDIR}/${FILE}"
 
 FILES=${ROOT}/results/*/
+
+declare -a GEN=(10,13 20,23 30,50 250,270 480,500)
 cd ${ROOT}
 
-for f in $FILES
+for f in ${FILES}
 do
-  $CMD "$f"
+    for i in ${GEN[@]} ;
+    do
+        declare -a V=$(IFS=$','; set ${i}; echo "$1 $2")
+        ${CMD} ${V[@]} ${f}
+    done
 done
